a1f5c5f6af8d4a0141cc834c364b3e196b99c6e5
yasser rachih <yassirach17@gmail.com>
2025-06-02 16:54:44 +0100

    file 1

---
+++ loan-service/src/test/java/com/booknet/loan_service/LoanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booknet.loan_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
